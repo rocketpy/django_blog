7005860df32e14134ba388b1e9315a7a128c9cd5
rocketpy <dimitr.ushakov@gmail.com>
2019-08-30 20:05:38 +0300

    updated

---
+++ commands.py
@@ -0,0 +1,15 @@
+#  pip install Django
+
+#  python -m django --version
+
+#  need create a new directory for project , cd new_project
+#  django-admin startproject name_project
+
+# for run server , go to project directory
+# python manage.py runserver
+# in browser , insert  127.0.0.1:8000
+# stop server :  CTRL + C
+
+# create a new app
+# python manage.py startapp name_app
+
