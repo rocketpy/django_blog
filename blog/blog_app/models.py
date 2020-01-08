b8cd6d0f6fb6871871c32be69cc24f9593aa9fda
rocketpy <dimitr.ushakov@gmail.com>
2020-01-08 23:32:18 +0200

    modified

--- blog/blog_app/models.py
+++ blog/blog_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
@@ -19,3 +20,26 @@ class Post(models.Model):
 
 #  for update use:  python manage.py makemigrations blog_app
 # second command:  python manage.py migrate blog_app
+
+
+"""
+STATUS = (
+    (0,"Draft"),
+    (1,"Publish")
+)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now= True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+"""
