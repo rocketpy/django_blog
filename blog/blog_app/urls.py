24d7aa07765512e6305e1fb62f25e708751036af
rocketpy <dimitr.ushakov@gmail.com>
2020-01-10 22:26:52 +0200

    modified

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -5,3 +5,10 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+
+"""
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+    path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+]
+"""
