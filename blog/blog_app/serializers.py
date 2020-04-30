431941869df3c216124da82c4c27cd308fe7d242
rocketpy <dimitr.ushakov@gmail.com>
2020-04-30 23:35:27 +0300

    added main code

--- blog/blog_app/serializers.py
+++ blog/blog_app/serializers.py
@@ -1,1 +1,8 @@
+from rest_framework import serializers
+from .model import Language
 
+
+class LanguageSerializer(serializers.Modelserializer):
+    class Meta:
+        model = Language
+        fields = ('id', 'name', 'paradigm')
