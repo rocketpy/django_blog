f287f7dc99aea54cae9ad662ffefdd56520479f7
rocketpy <dimitr.ushakov@gmail.com>
2020-05-01 23:51:03 +0300

    updated

--- blog/blog_app/urls.py
+++ blog/blog_app/urls.py
@@ -2,6 +2,10 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
+
+router = routers.DefaultRouter()
+router.register('languages', views.LanguageView)
+
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
