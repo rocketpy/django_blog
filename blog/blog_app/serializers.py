7e0d40584b596196e3ab9ef8a452ec2031ec6f7d
rocketpy <dimitr.ushakov@gmail.com>
2020-05-02 22:59:20 +0300

    updated

--- blog/blog_app/serializers.py
+++ blog/blog_app/serializers.py
@@ -6,3 +6,12 @@ class LanguageSerializer(serializers.Modelserializer):
     class Meta:
         model = Language
         fields = ('id', 'name', 'paradigm')
+
+        
+# this will return url        
+"""
+class LanguageSerializer(serializers.HyperlinkedModelserializer):
+    class Meta:
+        model = Language
+        fields = ('id', 'url', 'name', 'paradigm')
+"""
